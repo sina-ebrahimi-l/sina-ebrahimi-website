f514ce6e5a853cf7e8b0ac3b0d367d4861a74b98
sina-ebrahimi-l <sina.ebrahimi.l@gmail.com>
2025-03-10 01:53:26 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can add JavaScript functions for dynamic features like form submission or animations
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Website Loaded");
+});
